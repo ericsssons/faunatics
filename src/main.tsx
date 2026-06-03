720f8c1629d75cf1a9d16201b129f1c5935ddff0
ericsssons <eric.zapata.mena@gmail.com>
2026-06-03 13:42:00 +0200

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "@tanstack/react-router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { getRouter } from "./router";
+import "./styles.css";
+
+const { router, queryClient } = (() => {
+  const r = getRouter();
+  return { router: r, queryClient: r.options.context.queryClient };
+})();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>
+);
