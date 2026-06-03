8d1d5a818b5a224fc23bd7288610645bcb451831
ericsssons <eric.zapata.mena@gmail.com>
2026-06-03 14:02:50 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,5 @@
-import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "@tanstack/react-router";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { router, queryClient } from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  </StrictMode>
+  <h1 style={{ color: "red", fontSize: 48 }}>REACT FUNCIONA</h1>
 );
